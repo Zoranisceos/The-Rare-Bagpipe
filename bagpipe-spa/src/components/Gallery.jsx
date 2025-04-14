327f28da79ef8bed468ce2bcf547bd0def544892
Zoranisceos <zoranisceos@gmail.com>
2025-04-15 00:37:21 +0300

    Настройка проекта

---
+++ bagpipe-spa/src/components/Gallery.jsx
@@ -0,0 +1,14 @@
+
+import image1 from '../assets/images/IMG_20250409_212824_9.jpg'
+import image2 from '../assets/images/IMG_20250409_213053_6.jpg'
+import image3 from '../assets/images/IMG_20250409_213127_3.jpg'
+import image4 from '../assets/images/IMG_20250409_213253_4.jpg'
+import image5 from '../assets/images/IMG_20250409_213324_1.jpg'
+
+const images = [
+  { id: 1, src: image1, alt: 'Волынка 1' },
+  { id: 2, src: image2, alt: 'Волынка 2' },
+  { id: 3, src: image3, alt: 'Волынка 3' },
+  { id: 4, src: image4, alt: 'Волынка 4' },
+  { id: 5, src: image5, alt: 'Волынка 5' }
+]
