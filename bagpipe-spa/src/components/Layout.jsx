f5e7f639b3ecac4af459b7d20443b86e73523f2e
Zoranisceos <zoranisceos@gmail.com>
2025-04-15 22:34:19 +0300

    addOutlet

--- bagpipe-spa/src/components/Layout.jsx
+++ bagpipe-spa/src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
 
 const Layout = ({ children }) => {
   return (
     <div className="container">
       <Navbar />
       <main>{children}</main>
+      <Outlet />
     </div>
   );
 };
