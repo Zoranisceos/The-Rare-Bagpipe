3ec19107102910deaa2d45e5a28d8856229787f2
Zoranisceos <zoranisceos@gmail.com>
2025-04-23 16:27:13 +0300

    addFixesStyles

--- bagpipe-spa/src/pages/BagpipePage.jsx
+++ bagpipe-spa/src/pages/BagpipePage.jsx
@@ -61,14 +61,14 @@ const BagpipePage = () => {
 
       <Modal show={showVideoModal} onClose={() => setShowVideoModal(false)}>
         <div className="video-container">
-          <embed
+          <iframe
             width="560"
             height="315"
             src={videoUrl}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
             allowFullScreen
             title="Видео о волынке"
-          ></embed>
+          ></iframe>
         </div>
       </Modal>
     </div>
